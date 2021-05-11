54e4ee65e0e49d7610132dd2ac1b9fa6f32c89c7
John FOLLY <follyjohn@outlook.fr>
2021-05-11 19:46:10 +0000

    first commit

---
+++ src/main/java/org/hermit/cinerex/CinerexApplication.java
@@ -0,0 +1,13 @@
+package org.hermit.cinerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinerexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinerexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hermit/cinerex/CinerexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hermit.cinerex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinerexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
